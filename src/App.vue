64efa6dd2a9b4e19eac079f099e1e137461faa05
Tom Staples <staples.2007@yahoo.co.uk>
2023-08-16 19:03:19 +0100

    added pitshop logo

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,13 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
+  <img src="./assets/images/pitShop-logo-light.svg" alt="pitshop logo" height="300" width="300" />
   <RouterView />
 </template>
 
-<style scoped></style>
+<style>
+img {
+  display: block;
+  margin: auto;
+}
+</style>
