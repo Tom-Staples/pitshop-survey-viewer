40c8abcb198a9ff38af832207a4a46d73a7fc979
Tom Staples <staples.2007@yahoo.co.uk>
2023-08-19 20:58:03 +0100

    app constants

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const SERVER_ENDPOINT = 'https://pitshop-survey.cyclic.app/'
+
+export const SURVEY_QUESTIONS = {
+  survey001: [
+    'Q1. What is your age?',
+    'Q2. What gender do you identify as?',
+    'Q3. Where do you think F1 merchandise is lacking? (Select any that apply)',
+    'Q4. Do you think F1 merchandise is too expensive?',
+    'Q5. How much would you be willing to spend on F1 mechandise?',
+    'Q6. Who is your favourite F1 driver?',
+    'Q7. Who is your favourite F1 team?',
+    'Q8. What is your preferred fit for clothing?',
+    "Q9. What's your usual size in clothing?",
+    'Q10. Would you prefer to buy driver or team merchandise?',
+    'Q11. Which of the following items would you purchase? (Select any that apply)',
+    'Q12. What colour scheme would you prefer?'
+  ]
+}
